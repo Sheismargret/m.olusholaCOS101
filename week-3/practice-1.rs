764093cac598b353c1cdb3bd2d3a7fa8e23834c3
sheismargret <margret.olushola@pau.edu.ng>
2024-10-28 19:46:35 +0100

    (week-3)

---
+++ week-3/practice-1.rs
@@ -0,0 +1,11 @@
+fn main() {
+	Let p:f64 = 520_000_000.0;
+	Let r:f64 = 10.0;
+	Let n:f64 = 5.0;
+
+	//simple interest
+	Let a = p * ( 1.0 +(r / 100.0)) * n;
+	println!("Amount is {}", a);
+	Let cI = a - p;
+	println!("Compound Interest is {}", cI);
+}
