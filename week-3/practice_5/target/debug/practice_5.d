55b4ecf661576ebe688fcb0f442e8e672c4d66a3
sheismargret <margret.olushola@pau.edu.ng>
2024-11-03 03:47:44 +0100

    (My thirdd commit

---
+++ week-3/practice_5/target/debug/practice_5.d
@@ -0,0 +1,1 @@
+C:\Users\USER\m.olusholaCSC101\week-3\practice_5\target\debug\practice_5.exe: C:\Users\USER\m.olusholaCSC101\week-3\practice_5\src\main.rs
---
+++ week-3/practice_6/target/debug/practice_6.d
@@ -0,0 +1,1 @@
+C:\Users\USER\m.olusholaCSC101\week-3\practice_6\target\debug\practice_6.exe: C:\Users\USER\m.olusholaCSC101\week-3\practice_6\src\main.rs
